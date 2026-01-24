b029d3c219a213c5cfbe181e9c9081e4a3d8a7bf
zakaria atya <zakaria.atya@gmail.com>
2026-01-25 00:14:52 +0100

    changement localhost  par ip

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001/api",
-});
+const envBaseUrl = process.env.REACT_APP_API_URL;
+const runtimeBaseUrl = `${window.location.protocol}//${window.location.hostname}:4000/api`;
+const baseURL =
+  envBaseUrl && !envBaseUrl.includes("localhost") ? envBaseUrl : runtimeBaseUrl;
+
+const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("eol_token");
